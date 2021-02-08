4964e525c1b16cf7be7e98366734f62318b60278
Dyego Soriano <dyegodf2@gmail.com>
2021-02-08 18:14:03 -0300

    refatorando componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 import { Container, Text } from './styles'
 
 interface ButtonProps {
+  destiny: string
   title: string
 }
 
-const Button: React.FC<ButtonProps> = ({ title }) => {
+const Button: React.FC<ButtonProps> = ({ title, destiny }) => {
+  const navigation = useNavigation()
+
   return (
-    <Container>
+    <Container onPress={() => navigation.navigate(destiny)}>
       <Text>{title}</Text>
     </Container>
   )
