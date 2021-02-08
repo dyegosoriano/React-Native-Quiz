c12b83ce2aa9754e0603798433e5570ccb683d08
Dyego Soriano <dyegodf2@gmail.com>
2021-02-08 12:04:26 -0300

    refatorando estilo global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components/native'
 export const Container = styled.View`
   background-color: #5e67f0;
 
-  padding: 16px;
+  padding: 32px;
   height: 100%;
   width: 100%;
 
