40ff24b21fb2b4e8efb54533365743e85e675b5a
Dyego Soriano <dyegodf2@gmail.com>
2021-02-08 18:36:05 -0300

    refatorando rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 
+import Punctuation from '../screens/Punctuation'
+import Question from '../screens/Question'
 import Home from '../screens/Home'
 
 const { Navigator, Screen } = createStackNavigator()
@@ -11,6 +13,8 @@ const Routes: React.FC = () => {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Home" component={Home} />
+        <Screen name="Question" component={Question} />
+        <Screen name="Punctuation" component={Punctuation} />
       </Navigator>
     </NavigationContainer>
   )
