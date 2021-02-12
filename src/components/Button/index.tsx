81c0a443089c61f4561c4c70d606b9dd7a956a80
Dyego Soriano <dyegodf2@gmail.com>
2021-02-12 10:02:15 -0300

    Refatorando componente Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import { Container, Text } from './styles'
+import { QuestionsContext } from '../../context/QuestionContext'
 
 interface ButtonProps {
   destiny: string
+  reset?: boolean
   title: string
 }
 
-const Button: React.FC<ButtonProps> = ({ title, destiny }) => {
+const Button: React.FC<ButtonProps> = ({ title, destiny, reset }) => {
+  const { handleResetScore } = useContext(QuestionsContext)
   const navigation = useNavigation()
 
+  function handleOnPress() {
+    if (reset) handleResetScore()
+    navigation.navigate(destiny)
+  }
+
   return (
-    <Container onPress={() => navigation.navigate(destiny)}>
+    <Container onPress={handleOnPress}>
       <Text>{title}</Text>
     </Container>
   )
